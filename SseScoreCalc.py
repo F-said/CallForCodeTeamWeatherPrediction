c6b5095ae139736e5ed9b45d6ab21a747b1cdb80
F-said <f.saidmuratov@yahoo.com>
2018-09-16 10:54:25 -0400

    Uploaded S_se calculation and dataset

--- SseScoreCalc.py
+++ SseScoreCalc.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+import math
 
 # Import hurricane data
 Hurricane_nyc = pd.read_csv("/Users/farukhsaidmuratov/PycharmProjects/CallForCodeTeamWeatherPrediction/HurricaneNYCDataSet.csv")
 
 # Drop data columns not relevant to severity in terms of socio-economics
-Hurricane_nyc = Hurricane_nyc.drop(labels=["Category_UL", "Humidity_UL(Relative Humidity)", "Total_Duration(estimated_hours)", "Name"], axis=1)
+Hurricane_nyc = Hurricane_nyc.drop(labels=["Category_UL", "Humidity_UL(Relative Humidity)", "Total_Duration(estimated_hours)", "Name", "SST_UL(F)", "WindSpeed_UL(mph)"], axis=1)
 
 # Extract date
 Hurricane_nyc["Date"] = Hurricane_nyc["Date"].str.slice(-4)
@@ -21,25 +22,23 @@ for y in years:
 freq = pd.DataFrame(freq.sort_index())
 
 # Create dataframe to hold years from 2000-2017
-S_h = pd.DataFrame()
-S_h["Year"] = list(range(2000, 2018))
+S_se = pd.DataFrame()
+S_se["Year"] = list(range(2000, 2018))
 
 freq = freq.reset_index()
 # Concatenate date
-S_h = pd.concat([S_h, freq], axis=1)
+S_se = pd.concat([S_se, freq], axis=1)
 
-S_h = S_h.drop(labels="index", axis=1)
+S_se = S_se.drop(labels="index", axis=1)
 
 # Rename frequency column
-S_h = S_h.rename(index=str, columns={"Date": "Frequency"})
+S_se = S_se.rename(index=str, columns={"Date": "Frequency"})
+S_se.to_csv(path_or_buf="S_se.csv", index=False)
 
+# Calculations for S_se done by hand 
+'''
 # Get distance from average frequency compared to that year
-S_h["Distance_From_Avg"] = np.where(S_h["Frequency"] > 0, (S_h["Frequency"] - S_h["Frequency"].mean()), 0)
+S_se["S_se"] = np.where(S_se["Frequency"] > 0, Hurricane_nyc["Fatalities"]**2 + Hurricane_nyc["Economic_Loss(USD million estimated)"]**2, 0)
 
-# Get Wind_Speed/SST measure since Wind Speed and SSt are inversely correlated
-# Just zero everything out and fill it out yourself its legit only 9 datapoints
-S_h["WindSpeed/SST"] = [0] * 18
-S_h["Category"] = [0] * 18
-
-S_h.to_csv(path_or_buf="S_h.csv", index=False)
-
+S_se.to_csv(path_or_buf="S_se.csv", index=False)
+'''
